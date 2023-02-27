f5c62ee3b75127f6a0761bedbb401804357f480a
hansen <huangsunyang@126.com>
2023-02-27 22:51:23 +0800

    lua script fix

--- package/scripts/lua/object.lua
+++ package/scripts/lua/object.lua
@@ -1,43 +1,48 @@
-NativeObject = {
-    new = function(self, t)
-        if t == nil then
-            t = {}
-        end
-
-        return setmetatable(t, {
-            __index = self,
-            __newindex = self
-        })
-    end
-}
 
 local NativeObjectMeta = {
     __index = function(t, key)
         local instance = rawget(t, "__instance")
+        local parent = rawget(t, "__parent")
         if instance == nil then
-            return nil
+            return parent and parent[key]
         end
+
         local value = instance[key]
+        if value == nil then
+            return parent and parent[key]
+        end
 
         if type(value) ~= "function" then
             return value
         end
 
         -- function value should pass __instance as first arg
-        return rawset(t, key, function(t, ...)
+        return rawset(t.__parent or t, key, function(t, ...)
             return value(t.__instance, ...)
         end)[key]
     end,
 
     __newindex = function(t, key, value)
-        -- if native value?
         local instance = rawget(t, "__instance")
-        if instance and getmetatable(instance) and getmetatable(instance)[key] then
-            rawget(t, "__instance")[key] = value
+        local metatable = getmetatable(instance)
+        if instance and metatable and metatable[key] then
+            instance[key] = value
         else
             rawset(t, key, value)
         end
     end
+}
+
+
+NativeObject = {
+    new = function(self, t)
+        if t == nil then
+            t = {}
+        end
+
+        t.__parent = self
+        return setmetatable(t, NativeObjectMeta)
+    end,
 }
 
 return setmetatable(NativeObject, NativeObjectMeta)
